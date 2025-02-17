86e76421fa1807c20f83aeb94e0aa1bcf60f6381
genku <koraygench@gmail.com>
2025-02-17 18:44:54 +0300

    adding book test

--- backend/src/app/book/book.controller.ts
+++ backend/src/app/book/book.controller.ts
@@ -1,5 +1,5 @@
 import { Request, Response, Router } from "express";
-import { getBooks } from "./book.service";
+import { addBook, getBooks } from "./book.service";
 
 const router = Router();
 
@@ -11,6 +11,19 @@ router.get('/books', async (req: Request, res: Response) => {
         console.log(error);
         res.status(500).json({ error: "Something went wrong while fetching books." });
     }
-  });
+
+});
+
+
+router.get('/b', async (req: Request, res: Response) => {
+    try {
+        const books = await addBook();
+        res.status(200).json(books);
+    } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: "Something went wrong while fetching books." });
+    }
+
+});
 
 export default router;
--- backend/src/app/book/book.service.ts
+++ backend/src/app/book/book.service.ts
@@ -8,3 +8,11 @@ export async function getBooks() {
         console.log(error);
     }; 
 }
+
+export async function addBook() {
+    try{
+        const book = await Book.create({title:"test", author: "test", isbn: "isbn", publisher:"publisher", genre:"genre", price:1, quantity:1, coverImage:"coverImage"});
+    } catch (error){
+        console.log(error);
+    }
+}
